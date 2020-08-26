89885a498d8e53792afc385663cbb3901e991fb8
Paul Winter <zimin.pavel.v@gmail.com>
2020-08-26 17:49:40 +0300

    созданы списки

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -17,6 +17,50 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<char> charList = new List<char>();
+            charList.Add('!');
+            charList.Add('@');
+            charList.Add('#');
+            charList.Add('$');
+            charList.Add('%');
+
+            foreach (char i in charList)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
+            Point point1 = new Point(numList[0], numList[0], charList[0]);
+            Point point2 = new Point(numList[0], numList[1], charList[1]);
+            Point point3 = new Point(numList[1], numList[0], charList[2]);
+            Point point4 = new Point(numList[1], numList[1], charList[3]);
+
+            List<Point> pointList = new List<Point>();
+            pointList.Add(point1);
+            pointList.Add(point2);
+            pointList.Add(point3);
+            pointList.Add(point4);
+
+            foreach(Point point in pointList)
+            {
+                point.Draw();
+            }
+
             Console.ReadLine();
         }
     }
