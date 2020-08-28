2923ccc6b347427aead0b2f984b38fffff36517c
Paul Winter <zimin.pavel.v@gmail.com>
2020-08-28 11:19:00 +0300

    last changes

---
+++ Snake/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Message
+    {
+        public HorisontalLine topLine = new HorisontalLine(26, 52, 8, '=');
+        public HorisontalLine bottomLine = new HorisontalLine(26, 52, 16, '=');
+        public string text = @"И Г Р А   О К О Н Ч Е Н А";
+        public string author = @"created by  Paul Winter";
+    }
+}
